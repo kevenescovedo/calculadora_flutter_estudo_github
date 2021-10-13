e9ef0f9d5de18fc261d4ffbe084c1260c3669e02
Keven Escovedo <kevenescovedo1916@gmail.com>
2021-10-13 14:30:42 -0300

    correção

--- lib/Calculator.dart
+++ lib/Calculator.dart
@@ -1,6 +1,8 @@
+import 'package:calculadora/Button.dart';
 import 'package:calculadora/Controller.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:flutter_dropdown/flutter_dropdown.dart';
 
 class Calculator extends StatefulWidget {
   @override
@@ -10,64 +12,89 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   Controller controller = Controller();
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(left: 20, right: 20),
-        child: SingleChildScrollView(child:Column(
+        body: Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: SingleChildScrollView(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          Form(
-            key: controller.formKey,
-            child: Column(
-              children: <Widget>[
-                Lottie.network(
-                    'https://assets2.lottiefiles.com/private_files/lf30_yrjfqjmo.json'),
-                SizedBox(height: 20,),
-                TextFormField(
-                  controller: controller.controllerText1,
-                    validator: controller.validator,
-                    /*controller: _controller */
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+            Form(
+              key: controller.formKey,
+              child: Column(
+                children: <Widget>[
+                  Lottie.network(
+                      'https://assets2.lottiefiles.com/private_files/lf30_yrjfqjmo.json'),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                      controller: controller.controllerText1,
+                      validator: controller.validator,
+                      /*controller: _controller */
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
                         hintText: "Digite o primeiro número:",
-                    )
-                ),
-                SizedBox(height: 30,),
-                TextFormField(
-                    controller: controller.controllerText2,
-                    validator: controller.validator,
-                  /*controller: _controller */
-                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ValueListenableBuilder(
+                      valueListenable: controller.operatorNofitifer,
+                      builder: (context, value, _) {
+                        return DropDown(
+                          items: [
+                            "soma",
+                            "multiplicação",
+                            "subtração",
+                            'divisão'
+                          ],
+                          hint: Text(value),
+                          onChanged: (value) {
+                            controller.operatorNofitifer.value = value;
+                          },
+                        );
+                      }),
 
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),),
-                      hintText: "Digite o segundo número:",
-                    )
-                ),
-                // Add TextFormFields and ElevatedButton here.
-              ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                      controller: controller.controllerText2,
+                      validator: controller.validator,
+                      /*controller: _controller */
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                        hintText: "Digite o segundo número:",
+                      )),
+                  // Add TextFormFields and ElevatedButton here.
+                ],
+              ),
             ),
-          ),
-         Padding(padding: EdgeInsets.only(top: 30),
-           child:  InkWell(
-             child: Container(
-                width: MediaQuery.of(context).size.width * 1,
-               height: 60,
-               decoration: BoxDecoration(
-                 color: Colors.deepPurpleAccent,
-
-                 
-               ),
-               child: Center(child:Text("Calcular conta", style: TextStyle(color: Colors.white))),
-
-             ),
-             onTap:(){ controller.formValidar(context);},
-           ),
-         )
-
-        ],),
+            RaisedButton(
+              padding: EdgeInsets.only(top: 30, bottom: 30),
+              color: Colors.purple,
+              onPressed: () {
+                controller.formValidar(context);
+              },
+              child: Text("="),
+            )
+          ],
+        ),
       ),
     ));
   }
